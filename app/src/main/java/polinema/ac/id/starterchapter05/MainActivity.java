5c691245c303e40b8d22831788db360055b1c332
Septa Kusumaningtyas <septakusumaningtyas@gmail.com>
2019-10-03 13:35:12 +0700

    Tugas

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -1,8 +1,15 @@
 package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.fragments.DipsFragment;
+import polinema.ac.id.starterchapter05.fragments.HandstandFragment;
+import polinema.ac.id.starterchapter05.fragments.PushupFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void handlerPushups(View view) {
+        Fragment PushupFragment=getSupportFragmentManager().findFragmentByTag("PUSHUP_FRAGMENT");
+
+        if(PushupFragment!=null && PushupFragment.isVisible()){
+
+        }
+        else {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.frameLayout,new PushupFragment(),"PUSHUP_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void handlerDips(View view) {
+        Fragment DipsFragment=getSupportFragmentManager().findFragmentByTag("DIPS_FRAGMENT");
+
+        if(DipsFragment!=null && DipsFragment.isVisible()){
+
+        }
+        else {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.frameLayout,new DipsFragment(),"DIPS_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
+    }
+
+    public void handlerHandstand(View view) {
+        Fragment HandstandFragment=getSupportFragmentManager().findFragmentByTag("HANDSTAND_FRAGMENT");
+
+        if(HandstandFragment!=null && HandstandFragment.isVisible()){
+
+        }
+        else {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_from_left,R.anim.enter_from_left,R.anim.exit_from_right);
+            fragmentTransaction.replace(R.id.frameLayout,new HandstandFragment(),"HANDSTAND_FRAGMENT");
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        }
     }
 }
